5ca9f4e041801827ef4d4d94e65b983f9749d263
Lara_Bell <j8cwsqe29e2p@gmail.com>
2018-10-13 09:15:39 +0900

    UITextFiledで文字を入力

--- ViewController.swift
+++ ViewController.swift
@@ -8,76 +8,66 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
-    private var myInfoDarkButton: UIButton!
-    private var myInfoLightButton: UIButton!
-    private var myAddButton: UIButton!
-    private var myDetailButton: UIButton!
-    private var mySystemButton: UIButton!
+    private var myTextField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // ボタンを生成する
-        myInfoDarkButton = UIButton(type: .infoDark)
-        myInfoLightButton = UIButton(type: .infoLight)
-        myAddButton = UIButton(type: .contactAdd)
-        myDetailButton = UIButton(type: .detailDisclosure)
-        mySystemButton = UIButton(type: .system)
-        
-        // ボタンの位置を指定する
-        let posX: CGFloat = self.view.frame.width / 2
-        myInfoDarkButton.layer.position = CGPoint(x: posX, y: 50)
-        myInfoLightButton.layer.position = CGPoint(x: posX, y: 100)
-        myAddButton.layer.position = CGPoint(x: posX, y: 150)
-        myDetailButton.layer.position = CGPoint(x: posX, y: 200)
-        
-        // Systemｐボタンのサイズ
-        let sWidth: CGFloat = 200
-        let sHeight: CGFloat = 50
-        
-        // Systemボタンを配置するx y座標
-        let sposX: CGFloat = self.view.frame.width / 2 - sWidth / 2
-        let sposY: CGFloat = 250
+        // x,yｎと幅の高さを設定
+        let tWidth: CGFloat = 200
+        let tHeight: CGFloat = 30
+        let posX: CGFloat = (self.view.bounds.width - tWidth) /  2
+        let posY: CGFloat = (self.view.bounds.height - tHeight) / 2
             
-        // ボタンの設置座標とサイズを設定する
-        mySystemButton.frame = CGRect(x: sposX, y: sposY, width: sWidth, height: sHeight)
+        // UITextFieldを作成
+        myTextField = UITextField(frame: CGRect(x: posX, y: posY, width: tWidth, height: tHeight))
         
-        // Systemボタンにタイトルを設定する
-        mySystemButton.setTitle("MySystemButton", for: .normal)
+        // 表示するテキストを代入
+        myTextField.text = "Hello TextField"
         
-        // タグを設定する
-        myInfoDarkButton.tag = 1
-        myInfoLightButton.tag = 2
-        myAddButton.tag = 3
-        myDetailButton.tag = 4
-        mySystemButton.tag = 5
+        // Delegateを自身に設定する
+        myTextField.delegate = self
         
-        // イベントを追加する
-        myInfoDarkButton.addTarget(self, action: #selector(ViewController.onclickMyButton(sender:)), for: .touchDown)
-        myInfoLightButton.addTarget(self, action: #selector(ViewController.onclickMyButton(sender:)), for: .touchDown)
-        myAddButton.addTarget(self, action: #selector(ViewController.onclickMyButton(sender:)), for: .touchDown)
-        myDetailButton.addTarget(self, action: #selector(ViewController.onclickMyButton(sender:)), for: .touchDown)
-        mySystemButton.addTarget(self, action: #selector(ViewController.onclickMyButton(sender:)), for: .touchDown)
+        // 枠を表示する
+        myTextField.borderStyle = .roundedRect
         
-        // ViewにLabelを追加
-        self.view.addSubview(myInfoDarkButton)
-        self.view.addSubview(myInfoLightButton)
-        self.view.addSubview(myAddButton)
-        self.view.addSubview(myDetailButton)
-        self.view.addSubview(mySystemButton)
+        // クリアボタンを追加
+        myTextField.clearButtonMode = .whileEditing
+        
+        // viewに追加
+        self.view.addSubview(myTextField)
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-    }
-    
-    @objc internal func onclickMyButton(sender: UIButton) {
-        print("onClickMyButton:")
-        print("sender.currentTitle: \(sender.currentTitle)")
-        print("sender.tag: \(sender.tag)")
+    /*
+     UITextFieldが編集された直前に呼ばれる
+     カーソルが表示されたタイミング
+     */
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        print("textFieldDidBeginEditing: \(textField.text!)")
     }
 
+    
+    /*
+     UITextFieldが編集された直後に呼ばれる
+     */
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        print("textFieldDidEndEditing: \(textField.text!)")
+    }
+    
+    /*
+     改行ボタンが押された際に呼ばれる
+     */
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        print("textFieldShouldReturn: \(textField.text!)")
+        
+        // 改行ボタンが押されたらKeyboardを閉じる処理
+        textField.resignFirstResponder()
+        
+        return true
+    }
+    
+    
 }
 
