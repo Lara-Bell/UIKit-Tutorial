88fbda39dd39e8a22c670ef3b8e24c4a8cc1cb18
Lara_Bell <j8cwsqe29e2p@gmail.com>
2018-10-13 12:02:47 +0900

    UITableViewでテーブルを表示

--- ViewController.swift
+++ ViewController.swift
@@ -8,36 +8,57 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
-    private var myImageView: UIImageView!
+    private let myItems: NSArray = ["TEST1", "TEST2", "TEST3"]
+    private var myTableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         // サイズ設定
-        let iWidth: CGFloat = 300
-        let iHeight: CGFloat = 100
+        let barHeight: CGFloat = UIApplication.shared.statusBarFrame.size.height
         
-        // x, y を設定
-        let posX: CGFloat = (self.view.bounds.width - iWidth) /  2
-        let posY: CGFloat = (self.view.bounds.height - iHeight) / 2
-            
-        // UIImageView作成
-        myImageView = UIImageView(frame: CGRect(x: posX, y: posY, width: iWidth, height: iHeight))
+        let displayWidth: CGFloat = self.view.frame.width
+        let displayHeight: CGFloat = self.view.frame.height
         
-        // UIImage作成
-        let myImage: UIImage = UIImage(named: "sample")!
+        // TableViewの生成
+        myTableView = UITableView(frame: CGRect(x: 0, y: barHeight, width: displayWidth, height: displayHeight))
         
-        // UIImageViewに画像を設定
-        myImageView.image = myImage
+        // Cell名の登録をおこなう
+        myTableView.register(UITableViewCell.self, forCellReuseIdentifier: "MyCell")
+        
+        // DataSourceを自身に設定する
+        myTableView.dataSource = self
+        
+        // Delegateを自身に設定する
+        myTableView.delegate = self
         
         // viewに追加
-        self.view.addSubview(myImageView)
+        self.view.addSubview(myTableView)
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+    }
+    
+    /*
+     Cellの総数を返す
+     */
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return myItems.count
+    }
+    
+    /*
+     Cellに値を設定する
+     */
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // 再利用するCellを取得する
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MyCell", for: indexPath as IndexPath)
+        // Cellに値を設定する
+        cell.textLabel!.text = "\(myItems[indexPath.row])"
+        
+        return cell
     }
 }
 
